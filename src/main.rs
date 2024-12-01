5a7301bb907e3cdd1a464a649bcf9ffe4db19957
Emi <Emanuel.boehm@gmx.de>
2024-12-01 19:45:22 +0100

    make it nostd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+extern "C" fn _start() -> ! {
+    loop {}
+}
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
 }
