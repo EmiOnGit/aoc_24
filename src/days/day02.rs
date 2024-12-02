a5599975417d4eec9dbd7e39320a992af5dbfd60
Emi <Emanuel.boehm@gmx.de>
2024-12-02 21:02:41 +0100

    improve day2 part2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,41 +1,44 @@
+use arrayvec::ArrayVec;
+
 use crate::{print, println};
 
 pub fn part1(input: &str) {
+    let sum = input
+        .lines()
+        .map(line_to_report)
+        .map(is_report_valid)
+        .filter(Result::valid)
+        .count();
+    println!("total sum is ", sum);
+}
+fn line_to_report(input: &str) -> ArrayVec<i32, 128> {
+    input
+        .split_whitespace()
+        .map(str::parse::<i32>)
+        .flatten()
+        .collect()
+}
+pub fn part2(input: &str) {
     let mut sum = 0;
 
-    'shaft: for line in input.lines() {
-        let mut levels = line.split_whitespace().map(str::parse::<i32>).flatten();
-        let Some(mut prev_level) = levels.next() else {
-            continue 'shaft;
-        };
-        let mut dir = Dir::Unknown;
-        for level in levels {
-            let difference = level - prev_level;
-            if difference.abs() > 3 || difference == 0 {
-                continue 'shaft;
+    for report in input.lines().map(line_to_report) {
+        let result = is_report_valid(report.clone());
+        if result.valid() {
+            sum += 1;
+        } else {
+            let any_valid_route = (0..report.len())
+                .into_iter()
+                .map(|i| {
+                    let mut report = report.clone();
+                    report.remove(i);
+                    report
+                })
+                .map(is_report_valid)
+                .any(|arg0: Result| Result::valid(&arg0));
+            if any_valid_route {
+                sum += 1;
             }
-            match dir {
-                Dir::Inc => {
-                    if difference < 0 {
-                        continue 'shaft;
-                    }
-                }
-                Dir::Dec => {
-                    if difference > 0 {
-                        continue 'shaft;
-                    }
-                }
-                Dir::Unknown => {
-                    if difference > 0 {
-                        dir = Dir::Inc;
-                    } else {
-                        dir = Dir::Dec
-                    }
-                }
-            }
-            prev_level = level;
         }
-        sum += 1;
     }
     println!("total sum is ", sum);
 }
@@ -44,35 +47,47 @@ enum Dir {
     Dec,
     Unknown,
 }
-pub fn part2(input: &str) {
-    let mut sum = 0;
-
-    'shaft: for line in input.lines() {
-        let levels = line.split_whitespace().map(str::parse::<i32>).flatten();
-        'direction: for dir in [Dir::Inc, Dir::Dec] {
-            let mut levels = levels.clone();
-            let Some(mut prev_level) = levels.next() else {
-                continue 'shaft;
-            };
-            let mut used_dampener = false;
-            for level in levels {
-                let difference = match dir {
-                    Dir::Inc => level - prev_level,
-                    Dir::Dec => prev_level - level,
-                    Dir::Unknown => panic!(),
-                };
-                if difference > 3 || difference <= 0 {
-                    if used_dampener {
-                        continue 'direction;
-                    }
-                    used_dampener = true;
-                    continue;
-                }
-                prev_level = level;
-            }
-            sum += 1;
-            continue 'shaft;
-        }
+#[derive(PartialEq)]
+enum Result {
+    Valid,
+    Invalid,
+}
+impl Result {
+    pub fn valid(&self) -> bool {
+        *self == Result::Valid
     }
-    println!("total sum is ", sum);
+}
+fn is_report_valid(levels: impl IntoIterator<Item = i32>) -> Result {
+    let mut levels = levels.into_iter();
+    let Some(mut prev_level) = levels.next() else {
+        return Result::Invalid;
+    };
+    let mut dir = Dir::Unknown;
+    for level in levels {
+        let difference = level - prev_level;
+        if difference.abs() > 3 || difference == 0 {
+            return Result::Invalid;
+        }
+        match dir {
+            Dir::Inc => {
+                if difference < 0 {
+                    return Result::Invalid;
+                }
+            }
+            Dir::Dec => {
+                if difference > 0 {
+                    return Result::Invalid;
+                }
+            }
+            Dir::Unknown => {
+                if difference > 0 {
+                    dir = Dir::Inc;
+                } else {
+                    dir = Dir::Dec
+                }
+            }
+        }
+        prev_level = level;
+    }
+    return Result::Valid;
 }
