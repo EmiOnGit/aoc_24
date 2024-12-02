9547018ca3b84f0f0303dcb97b991e128a5bae0c
Emi <Emanuel.boehm@gmx.de>
2024-12-02 19:38:35 +0100

    day2 part2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -44,4 +44,35 @@ enum Dir {
     Dec,
     Unknown,
 }
-pub fn part2(_input: &str) {}
+pub fn part2(input: &str) {
+    let mut sum = 0;
+
+    'shaft: for line in input.lines() {
+        let levels = line.split_whitespace().map(str::parse::<i32>).flatten();
+        'direction: for dir in [Dir::Inc, Dir::Dec] {
+            let mut levels = levels.clone();
+            let Some(mut prev_level) = levels.next() else {
+                continue 'shaft;
+            };
+            let mut used_dampener = false;
+            for level in levels {
+                let difference = match dir {
+                    Dir::Inc => level - prev_level,
+                    Dir::Dec => prev_level - level,
+                    Dir::Unknown => panic!(),
+                };
+                if difference > 3 || difference <= 0 {
+                    if used_dampener {
+                        continue 'direction;
+                    }
+                    used_dampener = true;
+                    continue;
+                }
+                prev_level = level;
+            }
+            sum += 1;
+            continue 'shaft;
+        }
+    }
+    println!("total sum is ", sum);
+}
